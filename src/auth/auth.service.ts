5165b02d6a7a19a790e41e0760925407532bffe4
vitorazevedop7 <vitorazevedo.p7@gmail.com>
2025-08-18 16:46:21 -0300

    feat: add DTO for authentication and update auth controller and service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 
-@Injectable({})
+@Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
     signup() {
